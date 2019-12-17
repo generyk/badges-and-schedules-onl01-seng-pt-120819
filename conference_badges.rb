20bb222f47e1041d9e96f2b5f24d982490582e3d
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-17 03:50:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,10 @@
 def badge_maker(name)
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  names.each do |name|
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |name|
     puts "Hello, my name is #{name}!"
   end
 end
+
+def assign_rooms()
   
   
