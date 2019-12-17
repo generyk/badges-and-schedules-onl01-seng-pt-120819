e01cc99e59c79888804f81b897711e4549bb988a
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-17 04:37:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  count = 0  
+def batch_badge_creator(name)
   names = []
   while count < speakers.length do names << "Hello, #{speakers}!"
   count += 1 
